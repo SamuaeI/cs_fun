4b83dd2f024ee527448021d6303b1805d44a5bf7
RSamuaeI <retardsamuaeljlemon@gmail.com>
2023-05-15 18:53:18 -0400

    Create Program.cs

---
+++ Guess game/Program.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            string secret = "Hotdog";
+            string? plrGuess;
+            int guess_num = 0;
+            int guess_limit = 3;
+
+            do
+            {
+                Console.Write("guess the secret word: ");
+                plrGuess = Console.ReadLine();
+                guess_num++;
+            } while(guess_num < guess_limit && plrGuess != secret);
+            if(plrGuess == secret)
+            {
+                Console.WriteLine("wow you win!!");
+            }
+            else
+            {
+                Console.WriteLine("you lose!!");
+            }
+            
+        }
+
+    }
+}
