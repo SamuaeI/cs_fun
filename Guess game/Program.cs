877385b040a48f79b167be3101003a5e906bd152
RSamuaeI <retardsamuaeljlemon@gmail.com>
2023-05-15 19:01:24 -0400

    Update Program.cs

--- Guess game/Program.cs
+++ Guess game/Program.cs
@@ -1,31 +1,30 @@
 using System;
 
-public class Program
-{
-    public void Main(string[] args)
-    {   
+namespace Hello
+    class Program
+    {
+        static void Main(string[] args)
+        {   
 
-        string secret = "Hotdog";
-        string plrGuess = "";
-        int guess_num = 0;
-        int guess_limit = 3;
+            string secret = "Hotdog";
+            string plrGuess = "";
+            int guess_num = 0;
+            int guess_limit = 3;
 
-        do
-        {
-            Console.Write("guess the secret word: ");
-            plrGuess = Console.ReadLine();
-            guess_num++;
-        } while(guess_num < guess_limit && plrGuess != secret);
-        if(plrGuess == secret)
-        {
-            Console.WriteLine("wow you win!!");
+            do
+            {
+                Console.Write("guess the secret word: ");
+                plrGuess = Console.ReadLine();
+                guess_num++;
+            } while(guess_num < guess_limit && plrGuess != secret);
+            if(plrGuess == secret)
+            {
+                Console.WriteLine("wow you win!!");
+            }
+            else
+            {
+                Console.WriteLine("you lose!!");
+            }
         }
-        else
-        {
-            Console.WriteLine("you lose!!");
-        }
-
     }
-
 }
-
